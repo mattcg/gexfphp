d20045b6dbea6bdd2315ef40c1909e1eee71b72a
Matthew Caruana Galizia <mattcg@gmail.com>
2013-03-29 16:07:30 -0600

    Add AttributeList tests

--- src/MattCG/Gexf/AttributeList.php
+++ src/MattCG/Gexf/AttributeList.php
@@ -2,23 +2,101 @@
 
 namespace MattCG\Gexf;
 
+use MattCG\Gexf\Attribute;
+use MattCG\Gexf\AttributeClass;
+use MattCG\Gexf\AttributeType;
+
 use Rhumsaa\Uuid\Uuid;
 
-class AttributeList {
+class AttributeList implements \Iterator, \ArrayAccess, \SeekableIterator, \Countable {
 
 	private $mode, $attrclass, $attrs;
+
+	function append($value) {
+		if (!($value instanceof Attribute)) {
+			throw new \InvalidArgumentException();
+		}
+
+		$this->attrs[$value->getId()] = $value;
+	}
+
+	/* Countable interface */
+
+	function count() {
+		return count($this->attrs);
+	}
+
+	/* Iterator interface */
+
+	function current() {
+		return current($this->attrs);
+	}
+
+	function key() {
+		return key($this->attrs);
+	}
+
+	function next() {
+		next($this->attrs);
+	}
+
+	function rewind() {
+		rewind($this->attrs);
+	}
+
+	function valid() {
+		return current($this->attrs) !== false;
+	}
+
+	/* ArrayAccess interface */
+
+	function offsetSet($offset, $value) {
+		if (!($value instanceof Attribute)) {
+			throw new \InvalidArgumentException();
+		}
+
+		if ($offset !== $value->getId()) {
+			throw new \InvalidArgumentException();
+		}
+
+		$this->attrs[$offset] = $value;
+	}
+
+	function offsetUnset($offset) {
+		unset($this->attrs[$offset]);
+	}
+
+	function offsetExists($offset) {
+		return isset($this->attrs[$offset]);
+	}
+
+	function offsetGet($offset) {
+		if ($this->offsetExists($offset)) {
+			return $this->attrs[$offset];
+		}
+	}
+
+	/* SeekableIterator interface */
+
+	function seek($position) {
+		if (!$this->offsetExists($position)) {
+			throw new \OutOfBoundsException();
+		}
+
+		while (key($this->attrs) !== $position) next($this->attrs);
+	}
 
 	public function __construct(AttributeClass $attrclass) {
 		$this->attrclass = $attrclass;
 		$this->attrs = array();
 	}
 
-	public function addAttribute($id, AttributeType $type, $title) {
-		$this->attrs[$id] = new Attribute((string) $id, $type, $title);
+	public function addAttribute($id, AttributeType $type, $title = null) {
+		$this->attrs[$id] = new Attribute($id, $type, $title);
 	}
 
-	public function createAttribute(AttributeType $type, $title) {
-		$this->addAttribute(Uuid::uuid4(), $type, $title);
+	public function createAttribute(AttributeType $type, $title = null) {
+		$this->addAttribute(Uuid::uuid4()->toString(), $type, $title);
 	}
 
 	public function getAttributeClass() {
---
+++ tests/src/MattCG/Gexf/AttributeListTest.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * @author Matthew Caruana Galizia <m@m.cg>
+ * @copyright Copyright (c) 2013, Matthew Caruana Galizia
+ */
+
+use MattCG\Gexf\Attribute;
+use MattCG\Gexf\AttributeList;
+use MattCG\Gexf\AttributeClass;
+use MattCG\Gexf\AttributeType;
+use MattCG\Gexf\Mode;
+
+use Rhumsaa\Uuid\Uuid;
+
+class AttributeListTest extends PHPUnit_Framework_TestCase {
+
+	/**
+	 * @expectedException PHPUnit_Framework_Error
+	 */
+	public function testAttributeClassIsRequired() {
+		$attrlist = new AttributeList();
+	}
+
+	public function testGetAttributeClass() {
+		$attrclass = new AttributeClass();
+		$attrlist = new AttributeList($attrclass);
+		$this->assertEquals($attrclass, $attrlist->getAttributeClass());
+	}
+
+	public function testModeIsInitiallyNull() {
+		$attrlist = new AttributeList(new AttributeClass());
+		$this->assertNull($attrlist->getMode());
+	}
+
+	public function testSetMode() {
+		$attrlist = new AttributeList(new AttributeClass());
+		$mode = new Mode();
+		$attrlist->setMode($mode);
+		$this->assertEquals($mode, $attrlist->getMode());
+	}
+
+	public function testSetAttribute() {
+		$id = 'someid';
+		$attrlist = new AttributeList(new AttributeClass());
+		$attr = new Attribute($id, new AttributeType());
+		$this->assertEquals(0, count($attrlist));
+		$this->assertFalse(isset($attrlist[$id]));
+		$attrlist[$id] = $attr;
+		$this->assertEquals(1, count($attrlist));
+		$this->assertTrue(isset($attrlist[$id]));
+		$this->assertEquals($attr, $attrlist[$id]);
+	}
+
+	/**
+	 * @expectedException InvalidArgumentException
+	 */
+	public function testSetAttributeOnlyAcceptsAttributeAsValue() {
+		$attrlist = new AttributeList(new AttributeClass());
+		$attrlist[0] = 0;
+	}
+
+	/**
+	 * @expectedException InvalidArgumentException
+	 */
+	public function testSetAttributeOffsetMustBeEqualToId() {
+		$attrlist = new AttributeList(new AttributeClass());
+		$attr = new Attribute('someid', new AttributeType());
+		$attrlist['someotherid'] = $attr;
+	}
+
+	public function testAddAttribute() {
+		$id = 'someid';
+		$title = 'Title';
+		$attrtype = new AttributeType(AttributeType::TYPE_INTEGER);
+		$attrlist = new AttributeList(new AttributeClass());
+		$this->assertFalse(isset($attrlist[$id]));
+		$attrlist->addAttribute($id, $attrtype, $title);
+		$this->assertTrue(isset($attrlist[$id]));
+		$this->assertInstanceOf('MattCG\Gexf\Attribute', $attrlist[$id]);
+		$this->assertEquals($id, $attrlist[$id]->getId());
+		$this->assertEquals($title, $attrlist[$id]->getTitle());
+		$this->assertEquals($attrtype, $attrlist[$id]->getAttributeType());
+	}
+
+	public function testCreateAttribute() {
+		$attrtype = new AttributeType(AttributeType::TYPE_INTEGER);
+		$attrlist = new AttributeList(new AttributeClass());
+		$attrlist->createAttribute($attrtype);
+		$attr = $attrlist->current();
+		$this->assertInstanceOf('MattCG\Gexf\Attribute', $attr);
+		$this->assertEquals($attr, $attrlist[$attr->getId()]);
+
+		try {
+			$uuid = Uuid::fromString($attr->getId());
+		} catch (\InvalidArgumentException $e) {
+			$this->fail('ID should be valid UUID.');
+		}
+	}
+}
